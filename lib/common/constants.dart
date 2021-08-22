c453f0ef7fa5ab320d48d0cc3ebbcb0af70aefcd
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2021-08-22 17:31:26 +0400

    models added

---
+++ lib/common/constants.dart
@@ -0,0 +1,10 @@
+
+class Constants {
+  static final String BASE_URL = "http://127.0.0.1/api";
+}
+
+class EndPoint {
+  static String get category => "/category/";
+  static String get content => "/news/";
+}
+
--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,16 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:nepali_sayari/common/constants.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'category.g.dart';
+
+
+@JsonSerializable()
 class Category extends ChangeNotifier {
   int id;
   String title;
   String description;
   String thumbnail;
+  List<Category> categoryList;
 
+  Category({required this.id, required this.title, required this.description, required this.thumbnail });
+
+  factory Category.fromJson(Map<String, dynamic> json) => _$CategoryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CategoryToJson(this);
 
   void getCategory() {
-
+    http.get(Uri.parse(Constants.BASE_URL + "$EndPoint.category")).then((value)  {
+      if(value.statusCode == 200) {
+        categoryList = json.decode(value.body)
+            .map((data) => Category.fromJson(data))
+            .toList();
+      }
+    }).catchError((err) => print("getCategory :: ${err.toString()}"));
     notifyListeners();
   }
-
-
 }
